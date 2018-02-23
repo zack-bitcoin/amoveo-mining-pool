aad5b1060cf1a7c27dc0538bb592f160da59bbad
zack <zack.bitcoin@gmail.com>
2018-02-23 18:41:48 -0300

    make the mining pool so it wont crash when I am rebooting the full node.

--- apps/amoveo_mining_pool/src/mining_pool_server.erl
+++ apps/amoveo_mining_pool/src/mining_pool_server.erl
@@ -29,7 +29,7 @@ time_now() ->
     element(2, now()).
 new_problem_internal() ->
     Data = {mining_data},
-    {ok, [F, S, Third]} = packer:unpack(talk_helper(Data, ?FullNode, 2)),
+    {ok, [F, S, Third]} = packer:unpack(talk_helper(Data, ?FullNode, 10)),
     #data{hash = F, nonce = S, diff = Third, time = time_now()}.
 problem() -> gen_server:call(?MODULE, problem).
 problem_api_mimic() -> 
@@ -80,14 +80,26 @@ talk_helper(Data, Peer, N) ->
     if 
         N == 0 -> 
             io:fwrite("cannot connect to server"),
-            1=2;
+	    io:fwrite(packer:pack(Peer)),
+	    io:fwrite(packer:pack(Data)),
+	    timer:sleep(2000),
+	    talk_helper(Data, Peer, 1);
+            %1=2;
         true -> 
             case talk_helper2(Data, Peer) of
                 {ok, {_Status, _Headers, []}} ->
+		    io:fwrite("first failure  mode \n"),
                     timer:sleep(200),
                     talk_helper(Data, Peer, N - 1);
                 {ok, {_, _, R}} -> R;
-                _ -> io:fwrite("\nYou need to turn on and sync your Amoveo node before you can mine. You can get it here: https://github.com/zack-bitcoin/amoveo \n"),
+                {error, timeout} -> 
+		    io:fwrite(packer:pack(Data)),
+		    io:fwrite("timeout error\n"),
+		    timer:sleep(1000),
+		    talk_helper(Data, Peer, N - 1);
+                X -> 
+		    io:fwrite(packer:pack(X)),
+		    io:fwrite("\nYou need to turn on and sync your Amoveo node before you can mine. You can get it here: https://github.com/zack-bitcoin/amoveo \n"),
                      1=2
             end
     end.
