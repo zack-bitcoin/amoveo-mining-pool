991cd43733aaa72ea8833eddb6bd3cf2fe158f18
zack <zack.bitcoin@gmail.com>
2018-03-05 23:01:59 -0300

    remove excess print statments from mining pool

--- apps/amoveo_mining_pool/src/http_handler.erl
+++ apps/amoveo_mining_pool/src/http_handler.erl
@@ -8,9 +8,10 @@ handle(Req, State) ->
     Data = packer:unpack(Data0),
     case Data of
 	{work, _, _} ->
-	    io:fwrite("work from IP "),
-	    io:fwrite(packer:pack(IP)),
-	    io:fwrite("\n");
+	    %io:fwrite("work from IP "),
+	    %io:fwrite(packer:pack(IP)),
+	    %io:fwrite("\n");
+	    ok;
 	_ -> ok
     end,
     D0 = doit(Data),
