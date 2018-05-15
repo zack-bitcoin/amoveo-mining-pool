8ed18ab8d1d5e97995c54e7f5ce15866c692c196
Olivier Williams <olvrwllms@gmail.com>
2018-05-15 00:08:24 -0400

    Added usability improvements

--- js/server.js
+++ js/server.js
@@ -2,33 +2,27 @@
 
 local_ip = [127,0,0,1];
 local_port = 8081;
-var server_ip = document.createElement("INPUT");
-server_ip.setAttribute("type", "text");
-//server_ip.value = "159.89.106.253";// server
-server_ip.value = document.URL.split("/")[2].split(":")[0];
-var server_ip_info = document.createElement("h8");
-server_ip_info.innerHTML = "channel_node ip : ";
-var server_port = document.createElement("INPUT");
-//server_port.value = "8080";// server
-server_port.value = document.URL.split(":")[2].substring(0, 4);
-server_port.setAttribute("type", "text");
-var server_port_info = document.createElement("h8");
-server_port_info.innerHTML = "port: ";
-document.body.appendChild(server_ip_info);
-document.body.appendChild(server_ip);
-document.body.appendChild(server_port_info);
-document.body.appendChild(server_port);
 
-document.body.appendChild(document.createElement("br"));
-var miner_location = document.createElement("h8");
-miner_location.innerHTML = "point your miner here: http://".concat(server_ip.value).concat(":").concat(server_port.value);
-document.body.appendChild(miner_location);
-document.body.appendChild(document.createElement("br"));
+// Define server, fee and min. payout
+var server_ip = "159.89.106.253";
+var server_port = "8080";
+var pool_fee_amount = "0";
+var pool_min_payout_amount = "0.5";
+
+// Find elements
+var miner_location = document.querySelectorAll('#miner_location')[0];
+var pool_fee = document.querySelectorAll('#pool_fee')[0];
+var pool_min_payout = document.querySelectorAll('#pool_min_payout')[0];
+
+// Replace values in dynamic elements
+miner_location.innerHTML = "http://" + server_ip + ":" + server_port;
+pool_fee.innerHTML = pool_fee_amount + "%";
+pool_min_payout.innerHTML = pool_min_payout_amount + " VEO";
 
 function get_port() {
-    return parseInt(server_port.value, 10);
+    return parseInt(server_port, 10);
 }
 function get_ip() {
     //return JSON.parse(server_ip.value);
-    return server_ip.value;
+    return server_ip;
 }
