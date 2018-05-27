9b518601fb47ec7158dad5284f9a1d15021b5012
zack <zack.bitcoin@gmail.com>
2018-05-27 12:58:56 -0400

    turn a filter back on so the logging wont be so bad

--- apps/amoveo_mining_pool/src/http_handler.erl
+++ apps/amoveo_mining_pool/src/http_handler.erl
@@ -6,8 +6,8 @@ terminate(_Reason, _Req, _State) -> ok.
 handle(Req, State) ->
     {ok, Data0, Req2} = cowboy_req:body(Req, [{length, 200}, {period, 200}]),
     {{IP, _}, Req3} = cowboy_req:peer(Req2),
-    %Bool = contains(hd("."), 150, Data0),
-    Bool = false,
+    Bool = contains(hd("."), 150, Data0),
+    %Bool = false,
     D = if
 	    Bool -> <<>>;
 	    true ->
