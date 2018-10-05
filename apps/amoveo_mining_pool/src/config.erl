d70cbb5ce4a6264404b08b95100ddd65e9891a05
zack <zack.bitcoin@gmail.com>
2018-10-05 16:28:40 -0400

    lower the block reward

--- apps/amoveo_mining_pool/src/config.erl
+++ apps/amoveo_mining_pool/src/config.erl
@@ -1,8 +1,8 @@
 -module(config).
 -compile(export_all).
 
-mode() -> production.
-%mode() -> test.
+%mode() -> production.
+mode() -> test.
 full_node() -> 
     case mode() of
 	test ->
@@ -16,7 +16,7 @@ external() ->
     Z = [hd("/")|[hd("0")|tl(tl(Y))]],
     lists:reverse(Z).
 pool_reward() -> {0, 100}.%this is the portion of the block reward that goes to the mining pool. It is a fraction {numerator, denominator}. for example {1, 9} would mean that 1/9th of the block reward is kept as a fee, and 8/9ths are paid to miners..
-block_reward() -> 100227592.
+block_reward() -> 64139933.%100227592.
 pubkey() -> "BCjdlkTKyFh7BBx4grLUGFJCedmzo4e0XT1KJtbSwq5vCJHrPltHATB+maZ+Pncjnfvt9CsCcI9Rn1vO+fPLIV4=". %Initially, this pubkey controls all the shares in the pool. About half of the first (rt() + 1) block rewards will go to this account. This is important so that we don't over-reward the miners of the first 10 blocks.
 %When you are ready to shut off your node, first do `accounts:final_reward().` this way you don't under-reward the miners of the last 10 blocks. Use the extra money you got from the first 10 blocks to afford to pay the miners of the last 10 blocks.
 share_block_ratio() -> 
