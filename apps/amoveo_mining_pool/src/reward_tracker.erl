d2a00f5a490d587d21d89c7404952d4f91fbca5e
zack <zack.bitcoin@gmail.com>
2024-10-04 14:14:08 -0400

    Anoher print statemnet

--- apps/amoveo_mining_pool/src/reward_tracker.erl
+++ apps/amoveo_mining_pool/src/reward_tracker.erl
@@ -43,16 +43,18 @@ handle_cast({did_work, Pub, Hash}, X) ->
     {noreply, X2};
 handle_cast({new_block, Hash}, X) -> 
     X2 = case dict:find(Hash, X) of
-        error -> 
+             error -> 
                  %io:fwrite("we did not find that block\n"),
                  X;
-        {ok, H = #h{rs = Rs}} -> 
+             {ok, H = #h{rs = Rs}} -> 
                  io:fwrite("block was mined by us\n"),
                  io:fwrite(base64:encode(Hash)),
                  io:fwrite("\n"),
                  Rs2 = pay_if_exists(Rs, Hash),
                  Hs2 = H#h{rs = Rs2},
-                 dict:store(Hash, Hs2, X)
+                 dict:store(Hash, Hs2, X);
+             _ -> io:fwrite("rewards tracker impossible error\n")
+                     
          end,
     {noreply, X2};
 handle_cast(save, X) -> 
