7bd096f5614f602e9cccb74b1bd192af54fee11e
zack <zack.bitcoin@gmail.com>
2024-10-05 15:56:09 -0400

    use the new api to get the different hash

--- apps/amoveo_mining_pool/src/rewards_pusher.erl
+++ apps/amoveo_mining_pool/src/rewards_pusher.erl
@@ -73,5 +73,5 @@ pay_rewards2(Start, End, ServerPub) ->
     %io:fwrite("hash is: "),
     %io:fwrite(base64:encode(Hash)),
     %io:fwrite("\n"),
-    reward_tracker:new_block(hash:doit(Hash)),
+    reward_tracker:new_block(Hash),
     pay_rewards2(Start + 1, End, ServerPub).
